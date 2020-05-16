d889debc0dfa772d2c09dbe2cc1d93166521ac6d
binhn8 <bttt0808@gmail.com>
2020-05-16 13:17:41 +0700

    Test commit

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class HomeActivity extends Component {
         <View style={[styles.buttonContainer]}>
           <Button
             onPress={this.buttonClickListener}
-            title="Search"
+            title="Search 1"
             color="#00B0FF"
           />
         </View>
